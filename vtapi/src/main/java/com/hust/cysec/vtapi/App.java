529274dbdd577b194a952ff19c58080b21a7afed
qhwi <nguyenquochuy712@gmail.com>
2023-06-08 03:05:12 +0700

    add choose file modes

--- vtapi/src/main/java/com/hust/cysec/vtapi/App.java
+++ vtapi/src/main/java/com/hust/cysec/vtapi/App.java
@@ -1,7 +1,9 @@
 package com.hust.cysec.vtapi;
+// mvn clean compile assembly:single
 
 import java.io.IOException;
 import java.util.*;
+import java.io.File;
 
 public class App 
 {
@@ -14,7 +16,7 @@ public class App
         	System.out.println("***** Java VirusTotal API *****");
             System.out.println("1. Scan File\n2. Domain Analysis\n3. IP Analysis\n4. URL Analysis");
             System.out.println("*******************************");
-            System.out.print("Please choose: ");
+            System.out.print("> Please choose: ");
             
             @SuppressWarnings("resource")
 			Scanner input = new Scanner(System.in);
@@ -24,40 +26,52 @@ public class App
             }
             else{
             	System.out.println("ERROR: Invalid Input...\n");
+            	Thread.sleep(2000);
             	continue;
             }
             
             if (choice == 1) {
-            	System.out.println("STARTING: File Analysis... ");
+            	System.out.println("STARTING: File Analysis");
+            	System.out.println("\n>>> CHOOSE FILE <<<");
+            	System.out.println("Press ENTER to Browse Files...");
+            	System.out.print("Or Input filename (in this directory): ");
+            	String filename = input.nextLine().strip();
             	FileScan fs = new FileScan();
-            	UploadFile file = new UploadFile();
-            	fs.setFilepath(file.getFile());
+            	if (filename.isEmpty()) {
+                	UploadFile file = new UploadFile();
+                	fs.setFilepath(file.getFile());
+            	} else {
+            		File file = new File(filename);
+            		fs.setFilepath(file);
+            	}
                 
             	if (fs.isImported()) {
             		fs.POSTFile(true, API_KEY);
-            		System.out.println("> File ID: " + fs.getId());
+            		System.out.println("> Report ID: " + fs.getId());
             		fs.GETReport(API_KEY);
             		fs.printReport();
             	} else {
             		System.out.println("ERROR: No file imported!\n");
+            		Thread.sleep(2000);
             		continue;
             	}
                 break;
 
             } else if (choice == 2) {
-            	System.out.println("STARTING: Domain Analysis... ");
+            	System.out.println("STARTING: Domain Analysis");
             	//DomainReport();
             	break;
             } else if (choice == 3) {
-            	System.out.println("STARTING: IP Analysis... ");
+            	System.out.println("STARTING: IP Analysis");
             	//IPReport();
             	break;
             } else if (choice == 4) {
-            	System.out.println("STARTING: URL Analysis... ");
+            	System.out.println("STARTING: URL Analysis");
             	//URLReport();
             	break;
             } else
             	System.exit(0);
         } while (true);
+        System.exit(0);
     }
 }
